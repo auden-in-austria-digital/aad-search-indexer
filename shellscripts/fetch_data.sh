9a16c8d87f899925531635c8017902734ec107c3
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2024-06-20 12:56:41 +0200

    initial commit

---
+++ shellscripts/fetch_data.sh
@@ -0,0 +1,23 @@
+# bin/bash
+
+DATA_DIR=data/editions
+REPO=acdh-data
+ORG=acdh-oeaw
+BRANCH=main
+
+echo "fetching transkriptions from data repo..."
+rm -rf $DATA_DIR && mkdir $DATA_DIR
+curl -LO https://github.com/$ORG/$REPO/archive/refs/heads/$BRANCH.zip
+echo "data fetched"
+
+echo "unzipping data..."
+unzip $BRANCH
+
+echo "moving data to $DATA_DIR..."
+mkdir ./$DATA_DIR
+mkdir ./$DATA_DIR
+mv ./$REPO-$BRANCH/$DATA_DIR/*.xml ./$DATA_DIR
+
+rm -rf $REPO-$BRANCH
+rm $BRANCH.zip
+echo "done"
---
+++ typesense/utils.py
@@ -0,0 +1,30 @@
+ts_index_name = "gestrich_index"
+indexed_json = "indexed.json"
+
+
+def make_title(row):
+    date = row["Ausgabe"].split()[0]
+    y, m, d = date.split("-")
+    title = f"Wienerisches Diarium, {date}, Nummer {row['Nummer']}, Seite {row['Seite']}, laufende Nr. {row['ID']}"
+    return title
+
+
+def find_broader(row, lookup_table, index_nr):
+    item = row["full_code"][:index_nr]
+    try:
+        first_match = lookup_table[item]
+    except KeyError:
+        first_match = ""
+    return first_match
+
+
+def make_wr_id(row):
+    date = row["Ausgabe"].split()[0]
+    page = f"{row['Seite']:0>2}"
+    return f"wr_{date}__{page}"
+
+
+def set_default(obj):
+    if isinstance(obj, set):
+        return list(obj)
+    raise TypeError
